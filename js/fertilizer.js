dc9d6b67f5e3042ab7a01e14f17cad89a1a7c54f
iamtirthpatel <61939078+iamtirthpatel@users.noreply.github.com>
2021-05-28 00:04:13 +0530

    first commit

---
+++ js/fertilizer.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var input, filter, cards, cardContainer, h5, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-body h5.card-title");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
